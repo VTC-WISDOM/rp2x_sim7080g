69105375d72a024bd2ffe5579c10d0091cb959cd
amelia v <amelia@ag2v.com>
2025-07-17 13:38:39 -0400

    update config function and defs

--- src/rp2x_sim7080g.c
+++ src/rp2x_sim7080g.c
@@ -117,8 +117,42 @@ bool sim7080g_start(sim7080g_inst_t sim) {
 }
 
 bool sim7080g_config(sim7080g_inst_t sim, sim7080g_config_t config) {
-    
-    
+  uint8_t _cmd[128] = {0}; //command to send
+  uint8_t _exp[128] = {0}; //expected response
+  uint8_t _buf[128] = {0}; //received response buffer
+
+  sprintf(_cmd, "+CMGD=,4");
+  printf("%s", _cmd);
+  sim7080g_send_at_command(sim, _cmd);
+  while(!sim7080g_get_response(sim, _buf, sizeof(_buf))) sleep_ms(1);
+  printf("%s", _buf);
+  
+  sprintf(_cmd, "+CMEE=%i", config.sim_cmee);
+  printf("%s", _cmd);
+  sim7080g_send_at_command(sim, _cmd);
+  while(!sim7080g_get_response(sim, _buf, sizeof(_buf))) sleep_ms(1);
+  printf("%s", _buf);
+ 
+  sprintf(_cmd, "+CMGF=%i", config.sim_cmgf);
+  printf("%s", _cmd);
+  sim7080g_send_at_command(sim, _cmd);
+  while(!sim7080g_get_response(sim, _buf, sizeof(_buf))) sleep_ms(1);
+  printf("%s", _buf);
+ 
+  sprintf(_cmd, "+CNMP=%i", config.sim_cnmp);
+  printf("%s", _cmd);
+  sim7080g_send_at_command(sim, _cmd);
+  while(!sim7080g_get_response(sim, _buf, sizeof(_buf))) sleep_ms(1);
+  printf("%s", _buf);
+ 
+  sprintf(_cmd, "+CMNB=%i", config.sim_cmnb);
+  printf("%s", _cmd);
+  sim7080g_send_at_command(sim, _cmd);
+  while(!sim7080g_get_response(sim, _buf, sizeof(_buf))) sleep_ms(1);
+  printf("%s", _buf);
+ 
+  
+  
 }
 
 
@@ -168,3 +202,5 @@ bool sim7080g_power_on(sim7080g_inst_t sim) {
   return status;
 
 }
+
+
--- src/sim7080g_defs.h
+++ src/sim7080g_defs.h
@@ -2,7 +2,7 @@
 enum CMEE {
   cmee_disable = 0,
   cmee_numeric = 1,
-  cmee_vverbose = 2
+  cmee_verbose = 2
 };
 
 //sms format
@@ -25,6 +25,35 @@ enum CMNB {
   cmnb_both = 3
 };
 
+enum CFUN {
+  cfun_minimum = 0,
+  cfun_full = 1,
+  cfun_disable_phone = 4,
+  cfun_factory_test = 5,
+  cfun_reset = 6,
+  cfun_offline = 7
+};
+
+enum COPS_MODE {
+  cops_mode_auto = 0,
+  cops_mode_manual = 1,
+  cops_mode_manual_deregister = 2,
+  cops_mode_set_format = 3,
+  cops_mode_manual_auto = 4
+};
+
+enum CGREG_N {
+  cgreg_disable_unsolicited = 0,
+  cgreg_enable_unsolicited = 1,
+  cgreg_enable_gprs_rau = 4
+};
+
+//ip modes
+#define CGDCONT_IP "IP",
+#define CGDCONT_IPV6 "IPV6"
+#define CGDCONT_IPV4V6 "IPV4V6"
+#define CGDCONT_NONIP "Non-IP"
+
 typedef struct _sim7080g_inst_t {
   uart_inst_t *uart;
   uint uart_pin_tx;
@@ -34,8 +63,12 @@ typedef struct _sim7080g_inst_t {
 
 typedef struct _sim7080g_config_t {
   const char *apn;
+  const char *cgdcont_pdp_type;
   uint sim_cmee;
   uint sim_cmgf;
   uint sim_cnmp;
   uint sim_cmnb;
+  uint sim_cfun;
+  uint sim_cops_mode;
+  uint sim_cgreg;
 } sim7080g_config_t;
